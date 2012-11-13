3c150f33d5303b578dae1b289e605771fca550c7
artctrl <hammersmake@gmail.com>
2012-11-13 19:45:27 +1300

    added blend.py
    experimenting with functions from blendx.py

---
+++ blend.py
@@ -0,0 +1,31 @@
+'''
+Created on 11/11/2012
+
+@author: Will
+'''
+import blenx
+import random
+import autopy as ap
+
+def artcbot():
+    bleh.artct()
+    
+def blran():
+    xpos = random.randint(20, 750)
+    ypos = random.randint(150, 500)
+    
+
+    print xpos
+    print ypos
+
+def bltop():
+    xpos = random.randint(10, 580)    
+    ypos = random.randint(50, 300)
+    
+    print xpos
+    print ypos
+
+
+
+artcbot()  
+blran()
